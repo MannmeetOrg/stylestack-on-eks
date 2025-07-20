4441b406f2890cbafee2634156b93ddabbfe34a4
MannuA19 <mannmeet.ahir@gmail.com>
2025-07-20 22:35:07 +0530

    Terraform Code Updated

---
+++ create.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+mkdir -p {infra/{modules/{vpc,eks,rds,s3,sqs,elasticache,iam,network},environments/{dev,staging,prod}},k8s-manifests/{namespaces,deployments,services,ingress,configmaps-secrets,rbac,hpa},services/{frontend,webserver,application,api-gateway,business-logic,message-queue,data-access,cache,storage,infrastructure},scripts/utils}
+
+# Infra environment files
+touch  /infra/environments/dev/{main.tf,variables.tf,outputs.tf,terraform.tfvars,backend.tf}
+touch  /infra/environments/staging/.gitkeep
+touch  /infra/environments/prod/.gitkeep
+
+# Core project files
+touch  /infra/README.md
+touch  /scripts/{deploy.sh,destroy.sh}
+touch  /Jenkinsfile
+touch  /README.md
+touch  /.gitignore
+
+echo "✅ Project structure created. Next: git init & push to repo."
+
