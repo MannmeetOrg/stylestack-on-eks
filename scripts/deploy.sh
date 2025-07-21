461019df73bf0b6bfb2c44c6e1e3eebf017e975d
MannuA19 <mannmeet.ahir@gmail.com>
2025-07-21 23:25:02 +0530

    Deploy Project

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,7 +1,4 @@
-# scripts/deploy.sh
 #!/bin/bash
-set -e
 
-# Deploy Helm chart
-helm upgrade --install stylestack helm-charts/stylestack \
-  --namespace stylestack --create-namespace
+terraform init
+terraform apply -auto-approve
