c7c53f881862cad4677b6333512b18ec09db8610
MannuA19 <mannmeet.ahir@gmail.com>
2025-07-23 11:22:52 +0530

    Deploy Project

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -17,6 +17,11 @@ region = ap-south-1
 output = json
 EOL
 
+# Optional: export as env vars for Terraform
+export AWS_ACCESS_KEY_ID=${AWS_ACCESS_KEY_ID}
+export AWS_SECRET_ACCESS_KEY=${AWS_SECRET_ACCESS_KEY}
+export AWS_DEFAULT_REGION=ap-south-1
+
 echo "✅ AWS CLI configured successfully from deploy.sh"
 
 # Terraform Infrastructure Creation
