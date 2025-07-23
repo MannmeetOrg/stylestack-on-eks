98415e102076c5001be694ad1d13ae5231055c55
MannuA19 <mannmeet.ahir@gmail.com>
2025-07-23 11:16:37 +0530

    Deploy Project

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,5 +1,25 @@
 #!/bin/bash
 
+set -e
+
+# Setup AWS CLI config using GitHub Secrets
+mkdir -p ~/.aws
+
+cat > ~/.aws/credentials <<EOL
+[default]
+aws_access_key_id = ${AWS_ACCESS_KEY_ID}
+aws_secret_access_key = ${AWS_SECRET_ACCESS_KEY}
+EOL
+
+cat > ~/.aws/config <<EOL
+[default]
+region = ap-south-1
+output = json
+EOL
+
+echo "✅ AWS CLI configured successfully from deploy.sh"
+
+# Terraform Infrastructure Creation
 cd infra/
 terraform init
 terraform apply -auto-approve
